7b2048af6c28d6de6eb9ebbad214829a4c806217
KrystianMatkowski-GDsoft <christian.matkowski@gdsoft.info>
2023-12-15 17:29:51 +0100

    change id 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::API
   def current_user
-    @current_user ||= if doorkeeper_token
-                        User.find_by(id: doorkeeper_token[:resource_owner_id])
-                      else
-                        User.find_by(id: 1)
-                      end
+    @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id]) if doorkeeper_token
+
   end
 end
